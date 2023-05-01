1041af3dad3ce3e705496873113adaf83d4fc59a
ライガー <44018535+raiga0310@users.noreply.github.com>
2023-05-01 04:58:49 +0000

    ひとまずできたぞ

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import Head from 'next/head';
-import NavBar from '../components/Navbar';
+import { GetStaticProps } from 'next';
+import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import { markdownToHtml } from '../lib/markdownToHtml';
+import fs from 'fs';
+import path from 'path';
 
-const Profile: React.FC = () => {
+interface ProfileProps {
+  content: string;
+}
+
+const Profile: React.FC<ProfileProps> = ({ content }) => {
   return (
     <>
       <Head>
         <title>My Profile</title>
       </Head>
       <NavBar />
-      <h1>My Profile</h1>
-      <p>Welcome to my profile page!</p>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
       <Footer />
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const markdownPath = path.join(process.cwd(), 'markdown', 'profile.md');
+  const markdown = fs.readFileSync(markdownPath, 'utf-8');
+  const content = await markdownToHtml(markdown);
+
+  return {
+    props: {
+      content,
+    },
+  };
 };
 
 export default Profile;

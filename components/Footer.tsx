80130421db9c4f0bd5571b00658fec1a093b8283
ライガー <44018535+raiga0310@users.noreply.github.com>
2023-05-08 05:36:00 +0000

    chore: repository link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer>
       <p>© 2023 My Portfolio. All rights reserved.</p>
-      <a href="https://github.com/yourusername/yourrepository" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+      <a href="https://github.com/raiga0310/portfolio" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
     </footer>
   );
 };

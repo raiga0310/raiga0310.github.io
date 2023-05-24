d134dfaca42f8d78e97a9df2f5d2a55858981966
ライガー <44018535+raiga0310@users.noreply.github.com>
2023-05-24 09:56:35 +0000

    refactor(wip): markdown styling

--- pages/history.tsx
+++ pages/history.tsx
@@ -1,32 +1,29 @@
 import Head from 'next/head';
 import { GetStaticProps } from 'next';
-import { markdownToHtml } from '../lib/markdownToHtml';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import fs from 'fs';
 import path from 'path';
+import MarkdownContent from '../components/MarkdownContent';
 
-export default function Hitstory({ html }: { html: string }) {
+export default function Hitstory({markdown}: {markdown: string}) {
   return (
         <>
     <Head>
       <title>My Profile</title>
     </Head>
     <NavBar />
-    <div dangerouslySetInnerHTML={{ __html: html }} />
+    <MarkdownContent markdown={markdown}/>
     <Footer />
   </>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const markdownFilePath = path.join(process.cwd(), 'markdown', 'history.md');
-  const markdown = fs.readFileSync(markdownFilePath, 'utf8');
-  const html = await markdownToHtml(markdown);
-
+  const markdown = fs.readFileSync(path.join(process.cwd(), "markdown", "history.md"), "utf-8");
   return {
-    props: {
-      html,
-    },
+      props: {
+          markdown
+      }
   };
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,37 +3,46 @@ import Head from 'next/head';
 import { GetStaticProps } from 'next';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-import { markdownToHtml } from '../lib/markdownToHtml';
 import fs from 'fs';
 import path from 'path';
+import MarkdownContent from '../components/MarkdownContent';
 
 interface ProfileProps {
-  content: string;
+  markdown: string;
 }
 
-const Profile: React.FC<ProfileProps> = ({ content }) => {
+const Profile: React.FC<ProfileProps> = ({ markdown }: ProfileProps) => {
   return (
     <>
       <Head>
         <title>My Profile</title>
       </Head>
       <NavBar />
-      <div dangerouslySetInnerHTML={{ __html: content }} />
+      <MarkdownContent markdown={markdown}/>
       <Footer />
     </>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const markdownPath = path.join(process.cwd(), 'markdown', 'profile.md');
-  const markdown = fs.readFileSync(markdownPath, 'utf-8');
-  const content = await markdownToHtml(markdown);
-
-  return {
-    props: {
-      content,
-    },
-  };
+  const markdownPath = path.join(process.cwd(), "markdown", "profile.md");
+  console.log(markdownPath);
+  
+  if (!fs.existsSync(markdownPath)) {
+    console.log(`File does not exist: ${markdownPath}`);
+    return {
+      props: {
+          markdown: ""
+      }
+    };
+  } else {
+    const markdown = fs.readFileSync(markdownPath, "utf-8");
+    return {
+      props: {
+          markdown
+      }
+    };
+  }
 };
 
 export default Profile;

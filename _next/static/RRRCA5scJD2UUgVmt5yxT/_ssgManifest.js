2f0afecf79644a4f802f03673296afffaec9f49d
raiga0310 <raiga0310@users.noreply.github.com>
2023-06-19 06:29:53 +0000

    deploy: ef842282cd824a2f08c809552edeaec5f6b35703

---
+++ _next/static/RRRCA5scJD2UUgVmt5yxT/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblogs","\u002Fblogs\u002F[id]","\u002Fcontact","\u002Fhistory","\u002Fhobby","\u002Fskills"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
